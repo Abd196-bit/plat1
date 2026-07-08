943f31dc36359d6916cfe5caa67fe27a064add54
Abd196-bit <drimgirl87@gmail.com>
2026-07-08 17:10:01 +0530

    added animations and audio

---
+++ scripts/coin.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+signal collected
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		collect()
+
+
+func collect() -> void:
+	collected.emit()
+
+	# Prevent double-collection and hide the coin immediately
+	collision_shape_2d.set_deferred("disabled", true)
+	hide()
+
+	audio_stream_player_2d.play()
+	await audio_stream_player_2d.finished
+
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,40 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump: AudioStreamPlayer2D = $Jump
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump.play()
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("down", "up")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	update_animation(direction)
+
+
+func update_animation(direction: float) -> void:
+	# Flip the sprite based on movement direction.
+	if direction != 0:
+		animated_sprite_2d.flip_h = direction < 0
+
+	# Choose animation based on state.
+	if not is_on_floor():
+		animated_sprite_2d.play("jump")
+	elif direction != 0:
+		animated_sprite_2d.play("walking")
+	else:
+		animated_sprite_2d.play("idle")
