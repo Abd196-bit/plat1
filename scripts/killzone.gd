a6a36503bdbe4fcae103c7b91a6a1c91d52562ef
ArcadeLegendX <ArcadeLegendX@proton.me>
2026-07-09 10:02:18 -0400

    slime update

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,10 +5,13 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
+		body.get_node("CollisionShape2D").queue_free()
 		print("You Died")
+		Engine.time_scale = 0.5
 		timer.start()
 	
 	
 func _on_timer_timeout():
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
 	
