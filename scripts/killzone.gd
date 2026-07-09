7b12e2362912672e46b494e96f3a3a27cd30020c
ArcadeLegendX <ArcadeLegendX@proton.me>
2026-07-09 09:24:17 -0400

    green slime update

---
+++ scripts/killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@onready var timer = $Timer
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		print("You Died")
+		timer.start()
+	
+	
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
+	
---
+++ scripts/label.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var timer = $Timer
+
+
+func _on_body_entered(PLayer: Node2D) -> void:
+	print("You Died")
+	timer.start()
+	
+	
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
